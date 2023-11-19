edd69bb9643461bba7537431cd1374610e22a699
Gek91 <gek.pandini@gmail.com>
2023-11-19 07:34:13 +0100

    rest assured new example

--- rest-assured/src/test/simple/ControllerRestAssuredTest.java
+++ rest-assured/src/test/simple/ControllerRestAssuredTest.java
@@ -6,6 +6,7 @@ import io.restassured.RestAssured.*;
 import io.restassured.http.Header;
 import io.restassured.matcher.RestAssuredMatchers.*;
 import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -32,6 +33,17 @@ public class ControllerRestAssuredTest {
 	public void testGetByName() {
 		RestAssured.get("/examples/one").then().statusCode(200).assertThat()
 				.body("name", CoreMatchers.equalTo("one"));
+	}
+
+	@Test
+	public void testGetByNameFullObjectResultCheck() {
+
+		Example check = new Example("one", 1l, LocalDate.of(2023,9,10));
+
+		Example result = RestAssured.get("/examples/one").then().statusCode(200)
+				.extract().as(Example.class);
+
+		Assert.assertEquals(result, check);
 	}
 
 	@Test
