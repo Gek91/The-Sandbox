cdb1830d89e52b4adf60296b2eba432154cab010
Gek91 <gek.pandini@gmail.com>
2025-05-27 06:40:57 +0200

    fix spring api key example

--- spring/api-key/src/main/java/com/example/demo/DemoApplication.java
+++ spring/api-key/src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
+@SpringBootApplication(exclude = {})
 public class DemoApplication {
 
 	public static void main(String[] args) {
